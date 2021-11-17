a893c997d4cfe7f0d8f1de115e78b3c3965a4d2e
Phước IT <lethienphuoc@outlook.com>
2021-11-17 15:19:47 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NhungChiecFormTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.toolTipController1 = new DevExpress.Utils.ToolTipController(this.components);
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(91, 55);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(119, 13);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Đây là một label chữ nhỏ";
+            this.labelControl1.ToolTipController = this.toolTipController1;
+            this.labelControl1.ToolTipIconType = DevExpress.Utils.ToolTipIconType.Information;
+            // 
+            // toolTipController1
+            // 
+            this.toolTipController1.AllowHtmlText = true;
+            this.toolTipController1.AppearanceTitle.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolTipController1.AppearanceTitle.Options.UseFont = true;
+            this.toolTipController1.IconSize = DevExpress.Utils.ToolTipIconSize.Large;
+            this.toolTipController1.Rounded = true;
+            this.toolTipController1.GetActiveObjectInfo += new DevExpress.Utils.ToolTipControllerGetActiveObjectInfoEventHandler(this.toolTipController1_GetActiveObjectInfo);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 193);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.Utils.ToolTipController toolTipController1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NhungChiecFormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolTipController1_GetActiveObjectInfo(object sender, DevExpress.Utils.ToolTipControllerGetActiveObjectInfoEventArgs e)
+        {
+            try
+            {
+                if (e.SelectedControl != labelControl1) return;
+
+                string text = e.SelectedControl.Text;
+                SettingsToolTips((DevExpress.XtraEditors.LabelControl)e.SelectedControl,text);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void SettingsToolTips(DevExpress.XtraEditors.LabelControl ctl, string Decript)
+        {
+            try
+            {
+             
+
+                toolTipController1.ToolTipType = ToolTipType.Default;
+                toolTipController1.Appearance.Font = new System.Drawing.Font("Time New Roman", 12, FontStyle.Bold ^ FontStyle.Italic);
+                toolTipController1.Appearance.ForeColor = Color.Blue;
+                toolTipController1.Rounded = true;
+                toolTipController1.ShowBeak = true;
+                toolTipController1.ShowShadow = true;
+                toolTipController1.SetToolTip(ctl,string.Concat("Text  : ", Decript));
+                toolTipController1.SetTitle(ctl,"Detail Information: ");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
